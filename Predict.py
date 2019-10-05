3c79f65a89219525c1b06affc077b782afed8059
dio <wiixbox360ps4@gmail.com>
2019-10-05 09:33:19 +0800

    modified:   Predict.py

--- Predict.py
+++ Predict.py
@@ -8,7 +8,9 @@ ex = Experiment('Nested WaveUnet Prediction', ingredients=[config_ingredient])
 @ex.config
 def cfg():
     model_path = os.path.join("checkpoints", "125802", "125802-82000") # Load stereo vocal model by default
+    model_path = "./checkpoints/unet++_10_deep_supervised/588737-48000"
     input_path = os.path.join("audio_examples", "mix_p232_280.wav") # Which audio file to separate
+    input_path = "../Datasets/musdb18/test/AM Contra - Heart Peripheral.stem.mp4"
     output_path = None # Where to save results. Default: Same location as input.
 
 @ex.automain
